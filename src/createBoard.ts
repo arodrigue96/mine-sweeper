0de44908e860c9b7732b7e76ac2e76f4f00609c1
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-16 08:52:35 +0200

    Create createBoard and generateMine functions

---
+++ src/createBoard.ts
@@ -0,0 +1,17 @@
+import generateMine from "./generateMine.js";
+
+const createBoard = (numberOfCellsAndRows: number): Board => {
+  const board: Board = { cells: [] };
+
+  for (let row = 0; row < numberOfCellsAndRows; row++) {
+    board.cells.push([]);
+    for (let column = 0; column < numberOfCellsAndRows; column++) {
+      board.cells[row].push({
+        hasMine: generateMine(),
+        minesAround: 0,
+      });
+    }
+  }
+  debugger;
+  return board;
+};
