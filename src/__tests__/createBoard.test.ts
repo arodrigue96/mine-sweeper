bd0f25ba301bf828dd47f0f576155a1ade0cd8a6
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-17 23:51:18 +0200

    Fix createBoard.test.ts, repeated code and implementation fixes

--- src/__tests__/createBoard.test.ts
+++ src/__tests__/createBoard.test.ts
@@ -1,39 +1,35 @@
 import { createBoard } from "../board";
 
+const dimension = 5;
+const board = createBoard(dimension);
+
 describe("Given the createBoard function", () => {
   describe("When it receives 5", () => {
-    test("Then it should return a board with 5 rows and 5 columns", () => {
-      const dimension = 5;
-      const expectedBoardDimension = 5;
+    test("Then it should return a board with 5 rows", () => {
+      const expectedRows = 5;
+      const rows = board.length;
 
-      const board = createBoard(dimension);
-      const boardDimension = board.length;
-
-      expect(boardDimension).toBe(expectedBoardDimension);
+      expect(rows).toBe(expectedRows);
     });
   });
 
   describe("When it receives 5", () => {
-    test("Then it should return the first row has 5 cells.", () => {
-      const dimension = 5;
-      const expectedBoardRows = 5;
+    test("Then it should return a board where the first row has 5 cells.", () => {
+      const expectedCells = 5;
 
-      const rows = createBoard(dimension);
-      expect(rows.at(0)?.length).toBe(expectedBoardRows);
+      expect(board.at(0)?.length).toBe(expectedCells);
     });
   });
 
-  describe("When createBoard function is called", () => {
+  describe("When it is called", () => {
     test("Then it should return a board where each of its cell has not mines around", () => {
-      const dimension = 5;
       const expectedMinesAroundTotal = 0;
 
-      const board = createBoard(dimension);
       const allCellsHaveZeroMines = board.every((row) =>
         row.every((cell) => cell.minesAroundTotal === expectedMinesAroundTotal)
       );
 
-      expect(allCellsHaveZeroMines).toBeTruthy;
+      expect(allCellsHaveZeroMines).toBeTruthy();
     });
   });
 });
