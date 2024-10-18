5cd3b333b149fb5aba1677378becd2c0acba6b79
Aniol Rodriguez <arodrigue96@gmail.com>
2024-10-19 00:46:06 +0200

    Fix issues and reorganise code

---
+++ src/board/createBoard.test.ts
@@ -0,0 +1,30 @@
+import { createBoard } from "../board";
+
+const dimension = 5;
+
+describe("Given the createBoard function", () => {
+  describe("When it receives 5", () => {
+    test("Then it should return a board with 5 rows and where the first row has 5 cells.", () => {
+      const expectedRowsTotal = 5;
+      const expectedCellsTotal = 5;
+
+      const board = createBoard(dimension);
+
+      expect(board.length).toBe(expectedRowsTotal);
+      expect(board.at(0)?.length).toBe(expectedCellsTotal);
+    });
+  });
+
+  describe("When it is called", () => {
+    test("Then it should return a board where each of its cell has not mines around", () => {
+      const expectedMinesAroundTotal = 0;
+
+      const board = createBoard(dimension);
+      const allCellsHaveZeroMines = board.every((row) =>
+        row.every((cell) => cell.minesAroundTotal === expectedMinesAroundTotal)
+      );
+
+      expect(allCellsHaveZeroMines).toBeTruthy();
+    });
+  });
+});
